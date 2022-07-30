3b566fda9e5f4cfabafbc924cf2f66b118cbf83e
Gerard Hynes <gkhynes@gmail.com>
2022-07-30 19:06:57 +0100

    Optimize images with 11ty Image

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,29 @@
+const Image = require("@11ty/eleventy-img");
+
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [300, 800, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./public/images/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addPassthroughCopy("./src/images/");
+
+  eleventyConfig.addNunjucksAsyncShortcode("11tyImage", imageShortcode);
 
   return {
     dir: {
